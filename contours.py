457bcdc69d2b8942b74d8792e139054bbccc1f07
Quirina300 <70889288+Quirina300@users.noreply.github.com>
2021-06-25 10:21:56 +0200

    Added comments

--- contours.py
+++ contours.py
@@ -10,6 +10,7 @@ class contours():
         self.total_area = 0
 
     def filters(self):
+        # Function for filtering image
         image_blur = cv2.medianBlur(self.image,25)
         image_blur = cv2.GaussianBlur(image_blur,(5, 5), 0)
         image_blur_gray = cv2.cvtColor(image_blur, cv2.COLOR_BGR2GRAY)
@@ -19,12 +20,15 @@ class contours():
         self.opening = cv2.morphologyEx(image_thresh,cv2.MORPH_CLOSE,kernel) 
 
     def contours(self):
+        # Find the contours in the filtered image. Only find the outer contours and approximate the outer edges. 
+        # Then calculate the areas of all the contous. Then remove all the small contours.
         self.contour = cv2.findContours(self.opening.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         self.contour = imutils.grab_contours(self.contour)
         self.calculated_areas_of_contours()
         self.remove_small_contours()
 
     def calculated_areas_of_contours(self):
+        # Calculate for alle the found contours the area in square mm.
         for i, j in enumerate(self.contour):
             pixels = int(cv2.contourArea(self.contour[i]))
             area = pixels / 9.85  # square mm
@@ -32,6 +36,9 @@ class contours():
             self.area_list.append(area)
 
     def remove_small_contours(self):
+        # Remove all the small contours of less then 2000 mm^2
+        # First decide which contours to remove.
+        # Then remove the contours from the list with contours and areas
         for i, area in enumerate(self.area_list):
             if area < 2000:
                 self.index_to_remove.append(i)
@@ -41,6 +48,7 @@ class contours():
             del self.area_list[index]
        
     def display(self):
+        # Display all the contours on the original picture.
         for (i, c) in enumerate(self.contour):
             ((x, y), _) = cv2.minEnclosingCircle(c)
             cv2.putText(self.image, "#{}".format(i + 1), (int(x) - 45, int(y)+20),
@@ -48,14 +56,17 @@ class contours():
             cv2.drawContours(self.image, [c], -1, (0, 255, 0), 2)
 
     def get_amout_of_contours(self):
+        # Return the amout of contours.
         return(len(self.area_list))
 
     def get_areas_of_contours(self):
+        # Return the list with the found areas and the total area of all the contours.
         for index in range(len(self.area_list)):
             self.total_area = self.total_area + self.area_list[index]
         return(self.area_list, self.total_area)
 
     def show_image(self):
+        # Function for displaying the images.
         self.display()
         cv2.imshow('opening', self.opening)
         cv2.imshow('final', self.image)
